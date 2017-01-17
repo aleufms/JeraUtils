2a75671ed04feb2ff1514135f8441d06c6cb6f93
Alessandro Nakamuta <warthog@jera.com.br>
2017-01-17 14:20:23 -0300

    Adicionado fileUrlToGo ao webviewcontroller

--- JeraUtils/WebView/JeraWebViewController.swift
+++ JeraUtils/WebView/JeraWebViewController.swift
@@ -18,41 +18,73 @@ public enum JeraWebViewLinkAction{
 }
 
 public class JeraWebViewController: JeraBaseViewController {
-
-    public var urlToGo: NSURL? {
+    
+    public var urlToGo: URL? {
         didSet{
-            if let urlToGo = urlToGo {
-                webView.load(NSURLRequest(url: urlToGo as URL) as URLRequest)
+            goToUrlToGo()
+        }
+    }
+    
+    private func goToUrlToGo(){
+        if let urlToGo = urlToGo {
+            webView.load(URLRequest(url: urlToGo))
+        }
+    }
+    
+    public var fileUrlToGo: URL? {
+        didSet{
+            goToFileUrlToGo()
+        }
+    }
+    
+    private func goToFileUrlToGo(){
+        if let fileUrlToGo = fileUrlToGo{
+            if #available(iOS 9.0, *) {
+                // iOS9 and above. One year later things are OK.
+                webView.loadFileURL(fileUrlToGo, allowingReadAccessTo: fileUrlToGo)
+            } else {
+                // iOS8. Things can (sometimes) be workaround-ed
+                //   Brave people can do just this
+                //   fileURL = try! pathForBuggyWKWebView8(fileURL: fileURL)
+                //   webView.load(URLRequest(url: fileURL))
+                do {
+                    let fileURL = try fileURLForBuggyWKWebView8(fileURL: fileUrlToGo)
+                    webView.load(URLRequest(url: fileURL))
+                } catch let error as NSError {
+                    print("Error: " + error.debugDescription)
+                }
             }
         }
     }
     
     public var linkAction = JeraWebViewLinkAction.Default
-
+    
     public lazy var webView: JeraWebView = {
         let webView = JeraWebView()
         webView.backgroundColor = UIColor.clear
         webView.navigationDelegate = self
         return webView
     }()
-
+    
     fileprivate var disposeBag = DisposeBag()
-
+    
     override public func viewDidLoad() {
         super.viewDidLoad()
-
-//        screenName = "WebView"
-
+        
+        //        screenName = "WebView"
+        
         view.addSubview(webView)
         constrain(view, webView, block: { (view, webView) -> () in
             webView.edges == view.edges
         })
-
+        
         if let urlToGo = urlToGo {
-            webView.load(NSURLRequest(url: urlToGo as URL) as URLRequest)
+            goToUrlToGo()
+        }else if let fileUrlToGo = fileUrlToGo{
+            goToFileUrlToGo()
         }
     }
-
+    
     public func addShareButton(imageName imageName: String) {
         let shareBarButton = UIBarButtonItem(image: UIImage(named: imageName), style: .plain, target: nil, action: nil)
         
@@ -62,10 +94,35 @@ public class JeraWebViewController: JeraBaseViewController {
                     Helper.shareAction(url: contentURL as NSURL?, topViewController: strongSelf)
                 }
             }
-        }, onError: nil, onCompleted: nil, onDisposed: nil)
-        .addDisposableTo(disposeBag)
-
+            }, onError: nil, onCompleted: nil, onDisposed: nil)
+            .addDisposableTo(disposeBag)
+        
         navigationItem.rightBarButtonItem = shareBarButton
+    }
+    
+    //https://stackoverflow.com/questions/24882834/wkwebview-not-loading-local-files-under-ios-8
+    private func fileURLForBuggyWKWebView8(fileURL: URL) throws -> URL {
+        // Some safety checks
+        if !fileURL.isFileURL {
+            throw NSError(
+                domain: "BuggyWKWebViewDomain",
+                code: 1001,
+                userInfo: [NSLocalizedDescriptionKey: NSLocalizedString("URL must be a file URL.", comment:"")])
+        }
+        try! fileURL.checkResourceIsReachable()
+        
+        // Create "/temp/www" directory
+        let fm = FileManager.default
+        let tmpDirURL = URL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent("www")
+        try! fm.createDirectory(at: tmpDirURL, withIntermediateDirectories: true, attributes: nil)
+        
+        // Now copy given file to the temp directory
+        let dstURL = tmpDirURL.appendingPathComponent(fileURL.lastPathComponent)
+        let _ = try? fm.removeItem(at: dstURL)
+        try! fm.copyItem(at: fileURL, to: dstURL)
+        
+        // Files in "/temp/www" load flawlesly :)
+        return dstURL
     }
 }
 
@@ -84,7 +141,7 @@ extension JeraWebViewController: WKNavigationDelegate {
                     break
                 }
             }
-        }, onError: nil, onCompleted: nil, onDisposed: nil).addDisposableTo(disposeBag)
+            }, onError: nil, onCompleted: nil, onDisposed: nil).addDisposableTo(disposeBag)
     }
     
     public func webView(webView: WKWebView, decidePolicyForNavigationAction navigationAction: WKNavigationAction, decisionHandler: (WKNavigationActionPolicy) -> Void) {
@@ -96,7 +153,7 @@ extension JeraWebViewController: WKNavigationDelegate {
             case .Modal:
                 if let url = navigationAction.request.url{
                     let modalWebViewController = JeraWebViewController()
-                    modalWebViewController.urlToGo = url as NSURL?
+                    modalWebViewController.urlToGo = url
                     
                     let navigationController = JeraBaseNavigationController(rootViewController: modalWebViewController)
                     
@@ -125,20 +182,38 @@ extension JeraWebViewController: WKNavigationDelegate {
 }
 
 extension Helper {
-    public class func showWebViewWithURL(url: NSURL?, title: String? = nil, showShareButton: Bool = false, shareButtonImageName: String = "", presenterViewController: UIViewController) {
+    public class func showWebViewWithURL(url: URL?, title: String? = nil, showShareButton: Bool = false, shareButtonImageName: String = "", presenterViewController: UIViewController) {
         let jeraWebViewController = JeraWebViewController()
         jeraWebViewController.urlToGo = url
-
+        
         if let title = title {
-            jeraWebViewController.navigationItem.title = title
+            jeraWebViewController.title = title
         }
-
+        
         jeraWebViewController.addCloseButton()
-
+        
         if showShareButton || !shareButtonImageName.isEmpty {
             jeraWebViewController.addShareButton(imageName: shareButtonImageName)
         }
-
+        
+        let navigationController = JeraBaseNavigationController(rootViewController: jeraWebViewController)
+        presenterViewController.present(navigationController, animated: true, completion: nil)
+    }
+    
+    public class func showWebViewWithURL(fileUrl: URL?, title: String? = nil, showShareButton: Bool = false, shareButtonImageName: String = "", presenterViewController: UIViewController) {
+        let jeraWebViewController = JeraWebViewController()
+        jeraWebViewController.fileUrlToGo = fileUrl
+        
+        if let title = title {
+            jeraWebViewController.title = title
+        }
+        
+        jeraWebViewController.addCloseButton()
+        
+        if showShareButton || !shareButtonImageName.isEmpty {
+            jeraWebViewController.addShareButton(imageName: shareButtonImageName)
+        }
+        
         let navigationController = JeraBaseNavigationController(rootViewController: jeraWebViewController)
         presenterViewController.present(navigationController, animated: true, completion: nil)
     }
